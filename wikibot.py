87e26229f7ed18d92f88191922612a47889f93ae
Phuc Anh <anhtp1608@gmail.com>
2025-04-14 09:41:36 +0000

    Add test and wikibot

---
+++ wikibot.py
@@ -0,0 +1,12 @@
+from mylib.bot import scrape
+import click
+@click.command()
+@click.option('--name', prompt='Wikipedia page to scrape', 
+            help='Web page we want to scrape ')
+def cli(name):
+    result = scrape(name)
+    click.echo(click.style(f"{result}: ", fg="white"))
+
+if __name__ == '__main__':
+    cli()
+
